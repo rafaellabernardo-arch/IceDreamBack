c62b7f7aafc7b39ad447642ea1d459a3ab0e3a3d
RAFAELLA SOUZA BERNARDO <rafaella_bernardo@estudante.sesisenai.org.br>
2026-06-26 10:05:32 -0300

    cadastro de sorvetes

--- src/server.js
+++ src/server.js
@@ -18,8 +18,44 @@ app.post('/clientes', async (req, res) => {
     })
 
     res.json(cliente)
+}) 
+
+app.post('/produtos', async (req, res) => {
+
+    const { nome, preco } = req.body
+
+    const produto = await prisma.produto.create({
+        data: {
+            nome,
+            preco
+        }
+    })
+
+    res.json(produto)
+
 })
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000')
+})
+
+app.post('/pedidos', async (req, res) => {
+
+    const { clienteId, itens } = req.body
+
+    if (itens.length === 0) {
+        return res.status(400).json({
+            mensagem: 'O pedido deve possuir pelo menos um item'
+        })
+    }
+
+    const pedido = await prisma.pedido.create({
+        data: {
+            clienteId,
+            valorTotal: 0,
+            status: 'ABERTO'
+        }
+    })
+
+    res.json(pedido)
 })
